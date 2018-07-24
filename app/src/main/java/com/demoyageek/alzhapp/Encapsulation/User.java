44b99c4dc9946d4bd015bb291f462d361c4c1969
Stanley De Moya <standmv13@gmail.com>
2018-07-24 16:28:56 -0400

    video 9-12

---
+++ app/src/main/java/com/demoyageek/alzhapp/Encapsulation/User.java
@@ -0,0 +1,84 @@
+package com.demoyageek.alzhapp.Encapsulation;
+
+public class User {
+    private String name, email, password, code, isSharing, latitude, longitude, imageUrl;
+
+    public User(){
+    }
+
+    public User(String name, String email, String password, String code, String isSharing, String latitude, String longitude, String imageUrl) {
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.code = code;
+        this.isSharing = isSharing;
+        this.latitude = latitude;
+        this.longitude = longitude;
+        this.imageUrl = imageUrl;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getIsSharing() {
+        return isSharing;
+    }
+
+    public void setIsSharing(String isSharing) {
+        this.isSharing = isSharing;
+    }
+
+    public String getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(String latitude) {
+        this.latitude = latitude;
+    }
+
+    public String getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(String longitude) {
+        this.longitude = longitude;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+}
--- app/src/main/java/com/demoyageek/alzhapp/InviteCodeActivity.java
+++ app/src/main/java/com/demoyageek/alzhapp/InviteCodeActivity.java
@@ -2,20 +2,45 @@ package com.demoyageek.alzhapp;
 
 import android.content.Intent;
 import android.net.Uri;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class InviteCodeActivity extends AppCompatActivity {
+import com.demoyageek.alzhapp.Encapsulation.User;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class InviteCodeActivity extends AppCompatActivity implements View.OnClickListener {
     String name, email, password, isSharing,code;
     Uri imageUri;
     TextView txtCircleCode;
+    FirebaseAuth firebaseAuth;
+    FirebaseUser firebaseUser;
+    DatabaseReference databaseReference;
+    String userId;
+    Button btnCircleCodeDone;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_invite_code);
 
+        btnCircleCodeDone = findViewById(R.id.btnCircleCodeDone);
         txtCircleCode = findViewById(R.id.txtCircleCode);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        databaseReference = FirebaseDatabase.getInstance().getReference().child("Users");
 
         Intent inviteCodeIntent = getIntent();
         if (inviteCodeIntent != null) {
@@ -29,6 +54,36 @@ public class InviteCodeActivity extends AppCompatActivity {
         }
 
         txtCircleCode.setText(code);
-        
+        btnCircleCodeDone.setOnClickListener(this);
+    }
+
+    public void registerUser(View v){
+        firebaseAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+                    User newUser = new User(name, email, password, code, "false", "na", "na", "na");
+                    firebaseUser = firebaseAuth.getCurrentUser();
+                    userId = firebaseUser.getUid();
+
+                    databaseReference.child(userId).setValue(newUser).addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+                            if(task.isSuccessful()){
+                                Toast.makeText(getApplicationContext(), "Usuario Creado Satisfactoriamente", Toast.LENGTH_SHORT).show();
+                            }
+                            else {
+                                Toast.makeText(getApplicationContext(),"Error al crear usuario", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+                }
+            }
+        });
+    }
+
+    @Override
+    public void onClick(View v) {
+        registerUser(v);
     }
 }
