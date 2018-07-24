67d08aa168a029f4c64fc8307edfd2c10537b8e6
Stanley De Moya <standmv13@gmail.com>
2018-07-23 23:53:02 -0400

    Commit Video #7 Eligiendo la imagen y cortandola

--- app/src/main/java/com/demoyageek/alzhapp/MainActivity.java
+++ app/src/main/java/com/demoyageek/alzhapp/MainActivity.java
@@ -1,13 +1,36 @@
 package com.demoyageek.alzhapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    Button btnSignUp;
+    Button btnLogin;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnLogin = findViewById(R.id.btnLogin);
+        btnLogin.setOnClickListener(this);
+
+        btnSignUp = findViewById(R.id.btnSignUp);
+        btnSignUp.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if(view.getId() == R.id.btnSignUp) {
+            Intent intent = new Intent(MainActivity.this, RegisterActivity.class);
+            startActivity(intent);
+        }
+
+        if(view.getId() == R.id.btnLogin) {
+            Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+            startActivity(intent);
+        }
     }
 }
--- app/src/main/java/com/demoyageek/alzhapp/NameActivity.java
+++ app/src/main/java/com/demoyageek/alzhapp/NameActivity.java
@@ -2,12 +2,16 @@ package com.demoyageek.alzhapp;
 
 import android.content.Intent;
 import android.net.Uri;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import com.theartofdev.edmodo.cropper.CropImage;
+import com.theartofdev.edmodo.cropper.CropImageView;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -42,7 +46,7 @@ public class NameActivity extends AppCompatActivity implements View.OnClickListe
 
     @Override
     public void onClick(View v) {
-
+        generateCode(v);
     }
 
     public void generateCode(View v){
@@ -66,6 +70,31 @@ public class NameActivity extends AppCompatActivity implements View.OnClickListe
         }
         else{
             Toast.makeText(getApplicationContext(), "Por favor seleccione una imagen", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public void selectImage(View v){
+        Intent imageIntent = new Intent();
+        imageIntent.setAction(Intent.ACTION_GET_CONTENT);
+        imageIntent.setType("image/*");
+        startActivityForResult(imageIntent, 12);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == 12 && resultCode == RESULT_OK && data != null){
+            CropImage.activity()
+                    .setGuidelines(CropImageView.Guidelines.ON)
+                    .start(this);
+        }
+
+        if (requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE) {
+            CropImage.ActivityResult result = CropImage.getActivityResult(data);
+            if (resultCode == RESULT_OK) {
+                resultUri = result.getUri();
+            } else if (resultCode == CropImage.CROP_IMAGE_ACTIVITY_RESULT_ERROR_CODE) {
+                Exception error = result.getError();
+            }
         }
     }
 }
