4db89c8f71cb3090a5506dd1b69fff7a533ba2fc
Stanley De Moya <standmv13@gmail.com>
2018-07-24 22:03:38 -0400

    Video 17, Permisos de la aplicacion

--- app/src/main/java/com/demoyageek/alzhapp/MainActivity.java
+++ app/src/main/java/com/demoyageek/alzhapp/MainActivity.java
@@ -1,19 +1,25 @@
 package com.demoyageek.alzhapp;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.karan.churi.PermissionManager.PermissionManager;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     Button btnSignUp;
     Button btnLogin;
     FirebaseAuth firebaseAuth;
     FirebaseUser firebaseUser;
+    PermissionManager permissionManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +35,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
             btnSignUp = findViewById(R.id.btnSignUp);
             btnSignUp.setOnClickListener(this);
+
+            permissionManager = new PermissionManager() {};
+            permissionManager.checkAndRequestPermissions(this);
         }
         else{
             Intent navigationDrawerIntent = new Intent(MainActivity.this, NavigationDrawerActivity.class);
@@ -48,6 +57,17 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         if(view.getId() == R.id.btnLogin) {
             Intent intent = new Intent(MainActivity.this, LoginActivity.class);
             startActivity(intent);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        permissionManager.checkResult(requestCode, permissions, grantResults);
+
+        ArrayList<String> deniedPermissions = permissionManager.getStatus().get(0).denied;
+
+        if(deniedPermissions.isEmpty()){
+            Toast.makeText(getApplicationContext(), "Permisos Activados", Toast.LENGTH_SHORT).show();
         }
     }
 }
