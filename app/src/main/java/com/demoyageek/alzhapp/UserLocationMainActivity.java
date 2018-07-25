28d0b9b1723fcff39d15cb37184cf48e37edae9c
Stanley De Moya <standmv13@gmail.com>
2018-07-25 00:45:52 -0400

    Video 19, Posicion actual con poca precision

--- app/src/main/java/com/demoyageek/alzhapp/UserLocationMainActivity.java
+++ app/src/main/java/com/demoyageek/alzhapp/UserLocationMainActivity.java
@@ -1,9 +1,15 @@
 package com.demoyageek.alzhapp;
 
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -13,7 +19,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -24,9 +35,12 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 public class UserLocationMainActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener,OnMapReadyCallback {
+        implements NavigationView.OnNavigationItemSelectedListener, OnMapReadyCallback, GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, com.google.android.gms.location.LocationListener {
     private GoogleMap mMap;
     FirebaseAuth firebaseAuth;
+    GoogleApiClient googleApiClient;
+    LocationRequest locationRequest;
+    LatLng latLngLocation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -102,14 +116,11 @@ public class UserLocationMainActivity extends AppCompatActivity
 
         if (id == R.id.nav_joinCircle) {
             // Handle the camera action
-        }
-        else if (id == R.id.nav_myCircle) {
+        } else if (id == R.id.nav_myCircle) {
 
-        }
-        else if (id == R.id.nav_joinedCircle) {
+        } else if (id == R.id.nav_joinedCircle) {
 
-        }
-        else if (id == R.id.nav_signOut) {
+        } else if (id == R.id.nav_signOut) {
             FirebaseUser firebaseUser = firebaseAuth.getCurrentUser();
             if (firebaseUser != null) {
                 firebaseAuth.signOut();
@@ -128,9 +139,52 @@ public class UserLocationMainActivity extends AppCompatActivity
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        googleApiClient = new GoogleApiClient.Builder(this)
+                .addApi(LocationServices.API)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .build();
+
+        googleApiClient.connect();
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        locationRequest = new LocationRequest().create();
+        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        locationRequest.setInterval(3000);
+
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            return;
+        }
+        LocationServices.FusedLocationApi.requestLocationUpdates(googleApiClient, locationRequest, this);
+
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        if(locationRequest == null){
+            Toast.makeText(getApplicationContext(), "No se ha recibido Ubicacion", Toast.LENGTH_SHORT).show();
+        }
+        else {
+            latLngLocation = new LatLng(location.getLatitude(), location.getLongitude());
+
+            MarkerOptions markerOptions = new MarkerOptions();
+            markerOptions.position(latLngLocation);
+            markerOptions.title("Posicion Actual");
+
+            mMap.addMarker(markerOptions);
+        }
     }
 }
