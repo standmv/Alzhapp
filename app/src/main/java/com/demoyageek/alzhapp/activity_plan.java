13ea6ae7c6e0a879b595b57ba71501fb2eb38b0a
Jean Marte <jeancmarte21@gmail.com>
2018-07-26 02:08:19 -0400

    Activity salida planeada

---
+++ app/src/main/java/com/demoyageek/alzhapp/activity_plan.java
@@ -0,0 +1,13 @@
+package com.demoyageek.alzhapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class activity_plan extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_plan);
+    }
+}
